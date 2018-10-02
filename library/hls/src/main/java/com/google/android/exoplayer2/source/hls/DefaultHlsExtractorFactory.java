ed15101aff5eaba87e583bd5c3cf2516d83cc6a9
Eric Reed <spam@reedsplace.com>
2018-10-02 11:39:33 -0400

    Revert "Add AUD flag as well"
    This reverts commit 2f83e4409eaebefccf010f1910c68a0380300eb2.

--- library/hls/src/main/java/com/google/android/exoplayer2/source/hls/DefaultHlsExtractorFactory.java
+++ library/hls/src/main/java/com/google/android/exoplayer2/source/hls/DefaultHlsExtractorFactory.java
@@ -33,7 +33,6 @@ import java.util.Collections;
 import java.util.List;
 
 import static com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory.FLAG_ALLOW_NON_IDR_KEYFRAMES;
-import static com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory.FLAG_DETECT_ACCESS_UNITS;
 
 /**
  * Default {@link HlsExtractorFactory} implementation.
@@ -85,7 +84,7 @@ public final class DefaultHlsExtractorFactory implements HlsExtractorFactory {
     } else {
       // For any other file extension, we assume TS format.
       @DefaultTsPayloadReaderFactory.Flags
-      int esReaderFactoryFlags = DefaultTsPayloadReaderFactory.FLAG_IGNORE_SPLICE_INFO_STREAM | FLAG_ALLOW_NON_IDR_KEYFRAMES | FLAG_DETECT_ACCESS_UNITS;
+      int esReaderFactoryFlags = DefaultTsPayloadReaderFactory.FLAG_IGNORE_SPLICE_INFO_STREAM | FLAG_ALLOW_NON_IDR_KEYFRAMES;
       if (muxedCaptionFormats != null) {
         // The playlist declares closed caption renditions, we should ignore descriptors.
         esReaderFactoryFlags |= DefaultTsPayloadReaderFactory.FLAG_OVERRIDE_CAPTION_DESCRIPTORS;
